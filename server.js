833dda6ee32dc372d182bda0c1e681d73d3d300e
Kaiden Munsey <kaidenmunsey7@gmail.com>
2020-06-27 09:31:04 -0400

    finalizing set up for PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let path = require('path');
 let bodyParser = require('body-parser');
 
 //creating app port 
-let ap = express();
+let app = express();
 let PORT = 8080;
 
 //using app to show public file on html 
@@ -16,8 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
 //adding the routes to html and api pages 
-require(path.join(__dirname. './app/routing/apiRoutes.js'))(app);
-require(path.join(__dirname, './app/routing/htmlRoutes.js'))(app);
+require(path.join(__dirname, './app/routing/apiRoutes'));
+require(path.join(__dirname, '/app/routing/htmlRoutes'));
 
 //listening on port
 app.listen(PORT, function () {
