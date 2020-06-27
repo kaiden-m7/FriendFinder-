91a5923efe036bf826fa5245769c67a8fc5ede77
Kaiden Munsey <kaidenmunsey7@gmail.com>
2020-06-27 09:45:42 -0400

    adding friends array

---
+++ app/data/friends.js
@@ -0,0 +1,82 @@
+let friends = [
+    {
+        name: 'Gordon Ramsey',
+        photo: 'https://img.particlenews.com/img/id/2Ga7lb_0NlECUch00?type=thumbnail_1024x576',
+        score: [
+            
+
+        ]
+    }
+
+    {
+        name: 'Seth Rogen',
+        photo: 'https://pyxis.nymag.com/v1/imgs/c9e/734/2d121afa193a28deab95d6a1a473b8eb33-10-seth-rogen-2.rsquare.w700.jpg',
+        score: [
+
+        ]
+    }
+
+    {
+        name: 'Adam Sandler',
+        photo: 'https://storage.googleapis.com/afs-prod/media/033e139142424d38a618d680b1379270/3000.jpeg',
+        score: [
+
+        ]
+    }
+
+    {
+        name: 'Amy Poehler',
+        photo: 'https://m.media-amazon.com/images/M/MV5BYjIzMDA0NmUtMTlhZi00ZDU3LTkzYWMtZDhkYTU1ZDJlMGRkXkEyXkFqcGdeQXVyNzcwODE0OTk@._V1_.jpg',
+        score: [
+
+        ]
+    }
+
+    {
+        name: '',
+        photo: '',
+        score: [
+
+        ]
+    }
+
+    {
+        name: 'Aubrey Plaza',
+        photo: 'https://i.pinimg.com/originals/8d/20/25/8d2025d962e252b809b98b099e12b236.jpg',
+        score: [
+
+        ]
+    }
+
+    {
+        name: 'Sarah Paulson',
+        photo: 'https://cdn2.lamag.com/wp-content/uploads/sites/6/2017/12/sp_gg_preview.jpg',
+        score: [
+
+        ]
+    }
+
+    {
+        name: 'Ozzy Osborn',
+        photo: 'https://imageproxy.themaven.net//https%3A%2F%2Fwww.biography.com%2F.image%2FMTY5ODEzMTM4MzkxMTgxMTE1%2Fozzy-osbourne-gettyimages-660795784.jpg',
+        score: [
+
+        ]
+    }
+
+    {
+        name: 'George Strait',
+        photo: 'https://pbs.twimg.com/profile_images/1111371592641187841/b9SaYGLt_400x400.png',
+        score: [
+
+        ]
+    }
+
+    {
+        name: 'Dr. Phil',
+        photo: 'https://www.sun-sentinel.com/resizer/UqtSBP-1y3WvN01vyW2YfJ7Wvs8=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/CYTBIMFVRBGMJJ5VWNS7Q4JCTY.jpg',
+        score: [
+
+        ]
+    }
+]
