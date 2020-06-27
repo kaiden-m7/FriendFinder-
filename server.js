2c92df6b9b570ca5c4d3822aef394f324ac5ce5f
Kaiden Munsey <kaidenmunsey7@gmail.com>
2020-06-27 07:03:48 -0400

    creating port and linking routes

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
+
+//request dependencies 
 let express = require('express');
 let path = require('path');
 
+//creating app port 
 let ap = express();
-let PORT = process.env.PORT;
+let PORT = 8080;
+
+//using app to show public file on html 
+app.use(express.static(path.join(__dirname, './app/public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+//adding the routes to html and api pages 
+require(path.join(__dirname. './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+//listening on port
+app.listen(PORT, function () {
+    console.log('Friend Finder app listening on PORT: ' + PORT);
+});
