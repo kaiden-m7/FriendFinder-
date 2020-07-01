4512d10929ca230113fe3c9f92087ba4323168e2
Kaiden Munsey <kaidenmunsey7@gmail.com>
2020-07-01 18:10:33 -0400

    adding value to friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,16 @@ let friends = [
         name: 'Gordon Ramsey',
         photo: 'https://img.particlenews.com/img/id/2Ga7lb_0NlECUch00?type=thumbnail_1024x576',
         score: [
-            
+            '4',
+            '3',
+            '3',
+            '5',
+            '5',
+            '4',
+            '2',
+            '5',
+            '2',
+            '2'
 
         ]
     }
@@ -12,7 +21,16 @@ let friends = [
         name: 'Seth Rogen',
         photo: 'https://pyxis.nymag.com/v1/imgs/c9e/734/2d121afa193a28deab95d6a1a473b8eb33-10-seth-rogen-2.rsquare.w700.jpg',
         score: [
-
+            '4',
+            '2',
+            '2',
+            '3',
+            '4',
+            '5',
+            '1',
+            '2',
+            '5',
+            '5'
         ]
     }
 
@@ -20,7 +38,16 @@ let friends = [
         name: 'Adam Sandler',
         photo: 'https://storage.googleapis.com/afs-prod/media/033e139142424d38a618d680b1379270/3000.jpeg',
         score: [
-
+            '5',
+            '2',
+            '1',
+            '3',
+            '4',
+            '2',
+            '4',
+            '1',
+            '5',
+            '5'
         ]
     }
 
@@ -28,7 +55,16 @@ let friends = [
         name: 'Amy Poehler',
         photo: 'https://m.media-amazon.com/images/M/MV5BYjIzMDA0NmUtMTlhZi00ZDU3LTkzYWMtZDhkYTU1ZDJlMGRkXkEyXkFqcGdeQXVyNzcwODE0OTk@._V1_.jpg',
         score: [
-
+            '3',
+            '2',
+            '4',
+            '5',
+            '2',
+            '2',
+            '4',
+            '5',
+            '2',
+            '3'
         ]
     }
 
@@ -36,7 +72,16 @@ let friends = [
         name: 'Tina Fey',
         photo: 'https://media1.s-nbcnews.com/j/newscms/2017_33/2124466/170818-tina-fey-mn-0810_0064edfdad82f2a40ba5d6c2048fa6b9.fit-760w.jpg',
         score: [
-
+            '4',
+            '2',
+            '3',
+            '2',
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '3'
         ]
     }
 
@@ -44,7 +89,16 @@ let friends = [
         name: 'Aubrey Plaza',
         photo: 'https://i.pinimg.com/originals/8d/20/25/8d2025d962e252b809b98b099e12b236.jpg',
         score: [
-
+            '1',
+            '5',
+            '4',
+            '5',
+            '3',
+            '1',
+            '5',
+            '1',
+            '1',
+            '5'
         ]
     }
 
@@ -52,7 +106,16 @@ let friends = [
         name: 'Sarah Paulson',
         photo: 'https://cdn2.lamag.com/wp-content/uploads/sites/6/2017/12/sp_gg_preview.jpg',
         score: [
-
+            '1',
+            '2',
+            '4',
+            '5',
+            '2',
+            '3',
+            '4',
+            '4',
+            '5',
+            '1'
         ]
     }
 
@@ -60,7 +123,16 @@ let friends = [
         name: 'Ozzy Osborn',
         photo: 'https://imageproxy.themaven.net//https%3A%2F%2Fwww.biography.com%2F.image%2FMTY5ODEzMTM4MzkxMTgxMTE1%2Fozzy-osbourne-gettyimages-660795784.jpg',
         score: [
-
+            '2',
+            '3',
+            '1',
+            '5',
+            '5',
+            '1',
+            '3',
+            '3',
+            '1',
+            '5'
         ]
     }
 
@@ -68,7 +140,16 @@ let friends = [
         name: 'George Strait',
         photo: 'https://pbs.twimg.com/profile_images/1111371592641187841/b9SaYGLt_400x400.png',
         score: [
-
+            '4',
+            '2',
+            '3',
+            '5',
+            '1',
+            '2',
+            '3',
+            '2',
+            '5',
+            '5'
         ]
     }
 
@@ -76,7 +157,16 @@ let friends = [
         name: 'Dr. Phil',
         photo: 'https://www.sun-sentinel.com/resizer/UqtSBP-1y3WvN01vyW2YfJ7Wvs8=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/CYTBIMFVRBGMJJ5VWNS7Q4JCTY.jpg',
         score: [
-
+            '5',
+            '4',
+            '3',
+            '1',
+            '2',
+            '5',
+            '3',
+            '4',
+            '4',
+            '2'
         ]
     }
 ]
