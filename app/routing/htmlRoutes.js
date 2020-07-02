a24a70412231b19b3470b922749372ad73cd7f63
Kaiden Munsey <kaidenmunsey7@gmail.com>
2020-07-01 20:39:26 -0400

    adding html api for home and survey page

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,11 @@
+let path = require('path');
+
+module.export = function (app) {
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
+    
+    app.get('/survey', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/survery.html'));
+    });
+};
